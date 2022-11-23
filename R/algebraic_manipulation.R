6f6e04b0ec7ab82fc740680a92bd68bc6ffac57d
Pavel Novoa <pavel.novoa@gmail.com>
2022-11-23 13:16:12 +0100

    possibilities

--- R/algebraic_manipulation.R
+++ R/algebraic_manipulation.R
@@ -1,7 +1,10 @@
 
-#' Compute the score interval for each solution
+#' Scoring interval for each solution
 #'
-#' @param vert_eval_matrix An m by n matrix containing the scores of
+#' This function returns, for each solution, its range of possible scores
+#' (intervals).
+#'
+#' @param vertex_scores_matrix An m by n matrix containing the scores of
 #' each of the m solutions (rows) at each of the n vertices (columns).
 #'
 #' @return An m by 2 matrix containing the lower bound (LB) and
@@ -9,10 +12,11 @@
 #' @export
 #'
 #' @examples
-#' m <- matrix(c(1,2,3,4,3,2), byrow = T, nrow = 2)
+#' # Scoring two solutions on three criteria
+#' m <- matrix(c(1,2,3,4,3,2), byrow = TRUE, nrow = 2)
 #' compute_intervals(m)
-compute_intervals <- function(vert_eval_matrix){
-  intervals_bounds <- apply(vert_eval_matrix, MARGIN=1, FUN=range)
+compute_intervals <- function(vertex_scores_matrix){
+  intervals_bounds <- apply(vertex_scores_matrix, MARGIN=1, FUN=range)
   intervals_bounds <- t(intervals_bounds)
   colnames(intervals_bounds) <- c("LB", "UB")
   return(intervals_bounds)
---
+++ R/possibility_degree.R
@@ -0,0 +1,54 @@
+
+#' Degree of possibility that solution A is superior to solution B.
+#'
+#' Given the ranges of possible scores (intervals) of two solutions,
+#' this function returns the degree of possibility that solution A is
+#' superior to solution B.
+#'
+#' @param A a vector of the score range (interval) for the solution A. The
+#' first element is the lower dimension, while the second is the higher one.
+#' @param B a vector of the score range (interval) for the solution B and with
+#' the same structure as A.
+#' @param attitude attitude of the decision-maker. The three possible options
+#' are "neutral" (default), "optimistic", "pessimistic".
+#'
+#' @return a value between 0 and 1, where a value close to 0 means that
+#' solution A is inferior to B, and a value close to 1, the opposite.
+#' In case A = B, then the degree of possibility is 0.5.
+#' @export
+#'
+#' @examples
+#' A <- c(1, 3)
+#' B <- c(2, 4)
+#' # Neutral attitude
+#' compute_possibility_degree(A, B)
+#' # Pessimistic attitude
+#' compute_possibility_degree(A, B, attitude="pessimistic")
+#' # Optimistic attitude
+#' compute_possibility_degree(A, B, attitude="optimistic")
+compute_possibility_degree <- function(A, B, attitude="neutral"){
+
+  af = neutral_attitude
+  if(attitude == "pessimistic"){
+    af = pessimistic_attitude
+  } else{
+    af = optimistic_attitude
+  }
+
+  return(
+   ( af(A[2]) - af(B[1]) )/ ( af(A[2]) - af(A[1]) + af(B[2]) - af(B[1]) )
+  )
+}
+
+
+neutral_attitude <- function(x){
+  return(x)
+}
+
+pessimistic_attitude <- function(x){
+  return(log(x))
+}
+
+optimistic_attitude <- function(x){
+  return(x*sqrt(x))
+}
