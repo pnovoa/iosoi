65020fa13b0857564d98a77784c761eb9d01d75b
Pavel Novoa <pavel.novoa@gmail.com>
2022-11-23 15:53:42 +0100

    first version of soi identifier

--- R/possibility_degree.R
+++ R/possibility_degree.R
@@ -28,6 +28,13 @@
 #' compute_possibility_degree(A, B, attitude="optimistic")
 compute_possibility_degree <- function(A, B, attitude="neutral"){
 
+  if(A[2]<=B[1])
+    return(0)
+
+  if(A[1] >= B[2])
+    return(1)
+
+
   af = neutral_attitude
   if(attitude == "pessimistic"){
     af = pessimistic_attitude
@@ -38,6 +45,18 @@ compute_possibility_degree <- function(A, B, attitude="neutral"){
   return(
    ( af(A[2]) - af(B[1]) )/ ( af(A[2]) - af(A[1]) + af(B[2]) - af(B[1]) )
   )
+}
+
+
+compute_all_possibility_degrees <- function(interval_matrix, ref_sol, att_list = c("neutral")){
+
+  result <- sapply(att_list, function(att){
+    m <- apply(interval_matrix, MARGIN = 1, FUN = function(solA){
+      return(compute_possibility_degree(solA, ref_sol, attitude = att))
+    })
+  })
+
+  return(result)
 }
 
 
